0e210a0bd7b729eae155d8c657c942a3144be13b
Shahd El-Badrawy <91445925+Little-grow@users.noreply.github.com>
2024-06-03 21:59:45 +0300

    [103] [Time Beats: 100.00%] [Memory Beats: 28.30%] - LeetPush

--- 103-Binary-Tree-Zigzag-Level-Order-Traversal.cpp
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal.cpp
@@ -6,50 +6,50 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-   \
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        deque<TreeNode*> q;
         vector<vector<int>> res;
         if(!root)
             return res;
-    
+
+        deque <TreeNode*> q;
         q.push_back(root);
-        bool forwardLevel = true;
-        while(!q.empty()){
+        bool forward = true;
+
+        while (!q.empty()) {
             int sz = q.size();
-
-            vector<int> level; 
-
-            while(sz--){
-                TreeNode* cur; 
-                if(forwardLevel){
+            vector<int> level;
+            while (sz--) {
+                TreeNode* cur;
+                if (forward) {
                     cur = q.front();
                     q.pop_front();
-
-                    if(cur->left)
+                  
+                   if (cur->left)
                         q.push_back(cur->left);
-                    
-                    if(cur->right)
+
+                    if (cur->right)
                         q.push_back(cur->right);
-                }
-                else{
+
+                } else {
                     cur = q.back();
                     q.pop_back();
 
-                    if(cur->right)
+                    if (cur->right)
                         q.push_front(cur->right);
-
-                    if(cur->left)
+                   
+                    if (cur->left)
                         q.push_front(cur->left);
+                   
                 }
                 level.push_back(cur->val);
             }
-            forwardLevel = !forwardLevel;
+            forward = !forward;
             res.push_back(level);
         }
         return res;
