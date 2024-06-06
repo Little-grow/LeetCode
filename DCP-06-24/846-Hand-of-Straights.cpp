fe2f2f94fe081f3f29a47f9c0df7b7fd1553a394
Shahd El-Badrawy <91445925+Little-grow@users.noreply.github.com>
2024-06-06 23:38:11 +0300

    [846] [Time Beats: 25.47%] [Memory Beats: 54.60%] - LeetPush

--- DCP-06-24/846-Hand-of-Straights.cpp
+++ DCP-06-24/846-Hand-of-Straights.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int k) {
-        if(hand.size() % k != 0)
+    bool isNStraightHand(vector<int>& arr, int k) {
+        if (arr.size() % k != 0)
             return false;
-
-        map<int,int> m;
-        for(int i = 0; i < hand.size(); i++){
-            m[hand[i]]++;
+       
+        map<int, int> m;
+        for (int i = 0; i < arr.size(); i++) {
+            m[arr[i]] +=1;
         }
 
-        while(m.size() > 0)
-        {
-            int cur = m.begin()->first;
-            for(int i = 0; i < k; i++){
-                if(m[cur+i] == 0)
+        int cur;
+        while (m.size() > 0) {
+             cur = m.begin()->first;
+            for (int i = 0; i < k; i++) {
+                if (m[cur + i] == 0)
                     return false;
-                m[cur+i]--;
-                if(m[cur+i] == 0)
-                m.erase(cur+i);
+                --m[cur + i];
+                cout <<m[cur + i] <<'\n';
+                if (m[cur + i] == 0)
+                    m.erase(cur + i);
             }
         }
+
         return true;
     }
 };
